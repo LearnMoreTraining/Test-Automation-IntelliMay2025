b248cbb71511a6c989859251b669e3d4ec1f305a
LearnMoreTraining <anaravinthh@outlook.com>
2025-06-10 08:34:54 +0530

    loops

---
+++ Intelli7AMMay2025Training/src/test/java/javabasics/LoopsExample.java
@@ -0,0 +1,20 @@
+package javabasics;
+
+public class LoopsExample {
+
+    public static void main(String[] args) {
+
+        int [] a = {56,22,11,44};
+
+        for(int h=0; h < a.length ; h++ ){
+
+            System.out.println(a[h]);
+        }
+
+        String [] name = {"lmti","learnmore"};
+
+        for(int d=0 ; d < name.length ; d++){
+            System.out.println(name[d]);
+        }
+    }
+}
