bcb6cb57a075933ea0eecae2c5fd369d0d351d1f
LearnMoreTraining <anaravinthh@outlook.com>
2025-06-24 08:59:00 +0530

    css selector

--- Intelli7AMMay2025Training/src/main/java/pageobjects/SearchResultPage.java
+++ Intelli7AMMay2025Training/src/main/java/pageobjects/SearchResultPage.java
@@ -19,4 +19,12 @@ public class SearchResultPage {
 
             driver.findElement(By.xpath("//div[@data-cel-widget='search_result_"+value+"']/descendant::a[2]")).sendKeys(Keys.chord(Keys.CONTROL,Keys.ENTER));
     }
+
+    public void m1(){
+
+        driver.findElement(By.cssSelector("input[id='twotabsearchtextbox']")).click();
+        driver.findElement(By.cssSelector("#twotabsearchtextbox"));
+        driver.findElement(By.cssSelector(".input.r4.wide.mb16.mt8.username"));
+
+    }
 }
